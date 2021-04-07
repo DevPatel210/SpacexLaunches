84dc498d81624896d3e0ed4e98c6af4ad80406ef
DevPatel210 <devhpatel2@gmail.com>
2021-04-07 21:36:02 +0530

    App.test.js modified

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,3 @@ import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
